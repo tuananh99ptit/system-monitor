aea45c17591cca0e251db60d9ae8a725078df235
tuananh99ptit <tuananh1906.ptit@gmail,com>
2026-09-16 04:11:30 +0700

    feat integrate Memory monitor in to main

---
+++ build/CMakeFiles/system_monitor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/system_monitor.dir/src/memory/memory_monitor.cpp.o"
+  "CMakeFiles/system_monitor.dir/src/memory/memory_monitor.cpp.o.d"
+  "system_monitor"
+  "system_monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/system_monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/system_monitor.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/system_monitor.dir/src/main.cpp.o: \
+ /home/anhtran99hn/system-monitor/src/main.cpp /usr/include/stdc-predef.h
